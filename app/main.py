179b73bd75ab22e0d3430d0d0776deb6dc1225df
Medhansh Jain <medhanshjain2010@gmail.com>
2024-02-02 18:57:55 +0530

    added CreateUser Function

--- app/main.py
+++ app/main.py
@@ -102,3 +102,13 @@ def update_post(id: int, post: schemas.Postcreate, db: Session = Depends(get_db)
     db.commit()
 
     return existing_post.model_dump()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
